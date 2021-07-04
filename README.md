b11f938c921366d528bd99bfad67aee14d6e0aa6
Bedkow <bednarekdaniel@o2.pl>
2021-07-05 00:01:21 +0200

    1st

---
+++ README.md
@@ -0,0 +1,19 @@
+## brewing_calculator
+
+A single-page brewing calculator. Developed with JS, Sass and basic Vue 3 use, through CDN import.
+
+A calculator for the amount of sugar that needs to be added to wine/cider/beer [wort](https://en.wikipedia.org/wiki/Wort)/[must](https://en.wikipedia.org/wiki/Must) in order to get desired alcohol % and sweetness.
+Includes inputs for desired %, yeast's alcohol tolerance, and desired final volume in liters.
+
+Next possible features:
+
+- a list of fruits and their sugar content
+- [Balling scale](https://www.oxfordreference.com/view/10.1093/acref/9780195367133.001.0001/acref-9780195367133-e-107) support
+
+Using:
+
+- Vue.js 3
+- Webpack 5
+- Sass
+
+_under dev_
---
+++ src/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import './index.scss';
+
+// equation: 17 grams of sugar (const) x Desired volume (liters) x desired % of alcohol = x
+
+document.querySelector('.calc').addEventListener('click', function () {
+  const desiredAlcohol = Number(
+    document.querySelector('.desiredAlcohol').value,
+  );
+
+  // console.log(desiredAlcohol);
+
+  const desiredVolume = Number(document.querySelector('.volume').value);
+
+  // console.log(desiredVolume)
+
+  const outcome = 17 * desiredVolume * desiredAlcohol;
+
+  // 1kg of dissolved sugar has 600ml of volume
+  const outcomeText = `You need to add ${outcome} grams of sugar. This amount of sugar will add ${Math.trunc(
+    0.6 * outcome,
+  )} ml to the total volume.`;
+
+  document.querySelector('.sugarToAdd').textContent = outcomeText;
+
+  // changing styles TEST
+
+  // document.querySelector('#brewingCalc').style.borderColor = 'black';
+});
