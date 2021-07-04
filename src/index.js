fdfdfbfec3e72d75ab0f21b2ba02478f7d353931
Bedkow <bednarekdaniel@o2.pl>
2021-07-05 00:19:12 +0200

    uploading old project files to rework'em + basic webpack config

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 
-import './index.scss';
+import "./css/index.scss";
 
 // equation: 17 grams of sugar (const) x Desired volume (liters) x desired % of alcohol = x
 
-document.querySelector('.calc').addEventListener('click', function () {
-  const desiredAlcohol = Number(
-    document.querySelector('.desiredAlcohol').value,
-  );
+document.querySelector(".calc").addEventListener("click", function () {
+	const desiredAlcohol = Number(
+		document.querySelector(".desired-alcohol").value
+	);
 
-  // console.log(desiredAlcohol);
+	// console.log(desiredAlcohol);
 
-  const desiredVolume = Number(document.querySelector('.volume').value);
+	const desiredVolume = Number(document.querySelector(".volume").value);
 
-  // console.log(desiredVolume)
+	// console.log(desiredVolume)
 
-  const outcome = 17 * desiredVolume * desiredAlcohol;
+	const outcome = 17 * desiredVolume * desiredAlcohol;
 
-  // 1kg of dissolved sugar has 600ml of volume
-  const outcomeText = `You need to add ${outcome} grams of sugar. This amount of sugar will add ${Math.trunc(
-    0.6 * outcome,
-  )} ml to the total volume.`;
+	// 1kg of dissolved sugar has 600ml of volume
+	const outcomeText = `You need to add ${outcome} grams of sugar. This amount of sugar will add ${Math.trunc(
+		0.6 * outcome
+	)} ml to the total volume.`;
 
-  document.querySelector('.sugarToAdd').textContent = outcomeText;
+	document.querySelector(".sugar-to-add").textContent = outcomeText;
 
-  // changing styles TEST
+	// changing styles TEST
 
-  // document.querySelector('#brewingCalc').style.borderColor = 'black';
+	// document.querySelector('#brewingCalc').style.borderColor = 'black';
 });
